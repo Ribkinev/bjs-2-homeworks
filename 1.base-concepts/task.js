dd7cb2553fea4808b34f7aec7912cf488d624406
student <rybkinbot@gmail.com>
2025-03-18 21:31:21 +0300

    Initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,30 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr = [];
+  let discriminant = Math.pow(b, 2) - 4 * a * c;
   
-  return arr;
+  if (discriminant < 0) {
+    return arr;
+  } else if (discriminant === 0) {
+    let root = -b / (2 * a);
+    arr.push(root);
+    return arr;
+  } else {
+    let root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+    let root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+    arr.push(root1, root2);
+    return arr;
+  }
+
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
+  let monthlyRate = (percent / 100) / 12;
+  let loanBody = amount - contribution;
+  let monthlyPayment = loanBody * (monthlyRate + (monthlyRate / ((Math.pow((1 + monthlyRate),countMonths)) - 1)));
+  let totalAmount = monthlyPayment * countMonths;
+  return Math.round(totalAmount * 100) / 100;
+} 
+
+console.log (calculateTotalMortgage(15,0,10000,36));
