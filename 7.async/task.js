b582fb33668274b49f90b85cbac7d2e87422035d
student <rybkinbot@gmail.com>
2025-04-19 08:24:01 +0300

    Асинхронность

---
+++ 7.async/task.js
@@ -0,0 +1,61 @@
+class AlarmClock {
+    constructor() {
+        this.alarmCollection = [];
+        this.intervalId = null; 
+    }
+
+    addClock(time, callback) {
+        if(!time || !callback) {
+            throw new Error("Отсутствуют обязательные аргументы");
+        }
+
+        if (this.alarmCollection.some(alarm => alarm.time === time)) {
+            console.warn ("Уже присутствует звонок на это же время");
+            return;
+        }
+
+        this.alarmCollection.push( {
+            time: time,
+            callback: callback,
+            canCall: true
+        });
+    } 
+
+    removeClock(time) {
+        this.alarmCollection = this.alarmCollection.filter(alarm => alarm.time !== time);
+    } 
+
+    getCurrentFormattedTime() {
+        let date = new Date();
+        let hours = String(date.getHours()).padStart(2, '0');
+        let minutes = String(date.getMinutes()).padStart(2, '0');
+        return (hours+":"+minutes);
+    } 
+
+    start() {
+        if (this.intervalId) {
+        } else {this.intervalId = setInterval(() => {
+            this.alarmCollection.forEach(alarm => {
+                if (alarm.time === this.getCurrentFormattedTime() && alarm.canCall) {
+                    alarm.canCall = false;
+                    alarm.callback();
+                }
+            });
+            },1000);
+        }    
+    }
+
+    stop() {
+        clearInterval(this.intervalId);
+        this.intervalId = null;
+    }
+
+    resetAllCalls() {
+        this.alarmCollection.forEach(alarm => alarm.canCall = true);
+    }
+    
+    clearAlarms() {
+        this.stop();
+        this.alarmCollection = [];
+    }
+}
